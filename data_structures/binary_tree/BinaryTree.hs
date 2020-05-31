455a4878bc4e8110db4729d28e7f731c932f84d2
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2020-05-31 09:53:39 +0800

    Get levels, count nodes.

--- data_structures/binary_tree/BinaryTree.hs
+++ data_structures/binary_tree/BinaryTree.hs
@@ -1,6 +1,14 @@
 module BinaryTree where
 
+import qualified Data.List as L
+
 data BTree a = Empty | Node a (BTree a) (BTree a) deriving (Show)
+data Side = LeftSide | RightSide deriving (Eq, Show)
+
+-- Get subtree on specified side
+getSubTree :: Side -> BTree a -> BTree a
+getSubTree _ Empty = Empty
+getSubTree s (Node _ l r) = if s == LeftSide then l else r
 
 -- Get Left Subtree
 getLeftTree :: BTree a -> BTree a
@@ -17,11 +25,6 @@ nodeShow :: (Show a) => BTree a -> String
 nodeShow Empty = ""
 nodeShow (Node val _ _) = show val
 
--- Pretty Print a Binary Tree
-simplePrint :: (Show a) => BTree a -> String
-simplePrint Empty = ""
-simplePrint t = (nodeShow t) ++ " " ++ (simplePrint $ getLeftTree t) ++ (simplePrint $ getRightTree t)
-
 -- Depth first traversal
 dfsList :: BTree a -> [a]
 dfsList Empty = []
@@ -31,6 +34,7 @@ dfsList (Node n l r) = [n] ++ (dfsList l) ++ (dfsList r)
 bfsList :: BTree a -> [a]
 bfsList Empty = []
 bfsList t = concat $ takeWhile (\l -> (length l) > 0) [getLevel i 0 t | i <- [0..]]
+
 -- Get all nodes from a single level in the tree.
 getLevel :: (Num b, Enum b, Eq b) => b -> b -> BTree a -> [a]
 getLevel _ _ Empty = []
@@ -38,13 +42,37 @@ getLevel 0 _ (Node n l r) = [n]
 getLevel level i (Node n l r)
     | i == level = [n]
     | otherwise = (getLevel level (i+1) l) ++ (getLevel level (i+1) r)
---getLevel level i (Node n l r) = (if (i == level) then [n] else (getLevel level (i+1) l)) ++ (if i == level then [n] else getLevel level (i+1) r)
+
+-- Get a list of lists of nodes in each level
+getLevels :: BTree a -> [[a]]
+getLevels t = takeWhile (\l -> (length l) > 0) [getLevel i 0 t | i <- [0..]]
 
 -- Generate a Binary Tree from a list of values.
 -- Assume list is in breadth first order.
 fromList :: [a] -> BTree a
 fromList lst = fromListInt 0 lst
-
+-- Internal function to convert list to tree.
 fromListInt :: Int -> [a] -> BTree a
 fromListInt _ [] = Empty
-fromListInt i lst@(x:xs) = Node x (fromListInt (2*i + 1) (drop (i+1) lst)) (fromListInt (2*i + 2) (drop (i+2) lst))
+fromListInt i lst@(x:xs) = Node x (fromListInt (2*i + 1) (drop (i+1) lst)) 
+                                  (fromListInt (2*i + 2) (drop (i+2) lst))
+
+-- Append to left side of tree
+-- appendLeft :: BTree a -> BTree a
+-- Construct binary tree from a list of nodes and a specified set of sides.
+-- fromNodesAndSides :: a -> [a] -> [Side] -> BTree a
+-- fromNodesAndSides rt nodes sides = Node (head nodes) 
+
+-- Count number of nodes in the tree.
+numNodes :: BTree a -> Int
+numNodes t = length $ bfsList t
+
+
+-- Pretty Print a Binary Tree
+simplePrint :: (Show a) => BTree a -> String
+simplePrint Empty = ""
+simplePrint t = (nodeShow t) ++ " " ++ (simplePrint $ getLeftTree t) ++ (simplePrint $ getRightTree t)
+
+-- Better Pretty Printing of the binary tree.
+-- prettyPrint :: (Show a) => BTree a -> String
+
