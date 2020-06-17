5bfd0bc8425f5365f4c781f6164d3d1a9672beca
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2020-06-17 12:39:27 +0800

    Added some statistics functions

---
+++ src/Statistics/Center.hs
@@ -0,0 +1,22 @@
+module Statistics.Center where
+
+import qualified Data.Sort as S
+
+-- Measures of central tendency.
+
+arithmeticMean :: (Fractional a) => [a] -> a
+arithmeticMean vals = (sum vals)/(fromIntegral $ length vals)
+
+geometricMean :: (Floating a) => [a] -> a
+geometricMean vals = (product vals) ** (1/(fromIntegral $ length vals))
+
+harmonicMean :: (Fractional a) => [a] -> a
+harmonicMean vals = (sum $ map (1/) vals)/(fromIntegral $ length vals)
+
+median :: (Fractional a, Ord a) => [a] -> a
+median vals = if odd n 
+              then head $ drop mid vals 
+              else arithmeticMean $ take 2 $ drop (mid-1) vals
+    where sortedVals = (S.sort vals)
+          n = length vals
+          mid = n `div` 2 
