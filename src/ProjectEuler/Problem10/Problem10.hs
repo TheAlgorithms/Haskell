6432f9612aee308e667f67a9987dc2f19ead8f53
NoahSchiro <noahschiro@rocketmail.com>
2022-10-22 13:46:43 -0400

    Stylistic cleanup and comments

--- src/ProjectEuler/Problem10/Problem10.hs
+++ src/ProjectEuler/Problem10/Problem10.hs
@@ -1,12 +1,10 @@
--- The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+module ProjectEuler.Problem10.Problem10 where
 
--- Find the sum of all the primes below two million.
-
-module Main where
-
+-- Is a given integer n a prime?
 isPrime :: Integral a => a -> Bool
 isPrime n = n > 1 && null [() | k <- [2 .. floor $ sqrt $ fromIntegral n], mod n k == 0]
 
+-- Sum up all the primes between 1 and 2 million
 answer :: Integer
 answer = sum $ filter isPrime [1..2000000]
 
--- src/ProjectEuler/Problem9/Problem9.hs
+++ src/ProjectEuler/Problem9/Problem9.hs
@@ -1,18 +1,11 @@
--- A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
--- a2 + b2 = c2
+module ProjectEuler.Problem9.Problem9 where
 
--- For example, 32 + 42 = 9 + 16 = 25 = 52.
-
--- There exists exactly one Pythagorean triplet for which a + b + c = 1000.
--- Find the product abc.
-
-module Main where
-
+-- Brute force approach using list comprehensions
 answer :: Int
 answer = head [a*b*c | a <- [1..500],
                        b <- [1..500],
                        c <- [1..500], 
-                     a^2 + b^2 == c^2,
-                     a+b+c == 1000] 
+                     a^2 + b^2 == c^2, -- Filters
+                     a+b+c == 1000]    -- Filters
 
 main = do print answer
